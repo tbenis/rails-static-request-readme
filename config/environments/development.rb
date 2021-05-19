2e439dc97be542fce93c30d62e8123b9faf52065
Benis Tambe <will.jenifer@yahoo.com>
2021-05-18 22:16:23 -0400

    commit tree ed3e478eb5d03fa9a8dc5c816795a7158f04e2c1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.reload_classes_only_on_change = false #Added by Benis. To auto reload
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
